577986bd982ec6e61b7dc3720911231538009892
Derick de Jesus <derickcjesus@gmail.com>
2021-03-29 11:43:59 -0300

    feat: creates component TestifyItem

---
+++ src/components/TestifyItem/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import './styles.css'
+
+export default function TestifyItem() {
+    return (
+        <article className="testify-item">
+                <header>
+                    <strong>Derick de Jesus</strong>
+                    <span>21 anos</span>
+                </header>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Aut, sunt laboriosam 
+                    tenetur voluptatum cumque voluptate maxime
+                    consequatur nihil delectus accusantium
+                    officia nostrum maiores quia mollitia? 
+                    Delectus facilis saepe dicta molestias.
+                    Aut, sunt laboriosam tenetur voluptatum
+                    cumque voluptate maximeconsequatur nihil 
+                    delectus accusantiumofficia nostrum maiores
+                    quia mollitia? Delectus facilis saepe dicta 
+                    molestias. Aut, sunt laboriosam 
+                    tenetur voluptatum cumque voluptate maxime
+                    consequatur nihil delectus accusantium
+                    officia nostrum maiores quia mollitia? 
+                    Delectus facilis saepe dicta molestias.
+                    Aut, sunt laboriosam tenetur voluptatum
+                    cumque voluptate maximeconsequatur nihil 
+                    delectus accusantiumofficia nostrum maiores
+                    quia mollitia? Delectus facilis saepe dicta 
+                    molestias. Aut, sunt laboriosam 
+                    tenetur voluptatum cumque voluptate maxime
+                    consequatur nihil delectus accusantium
+                    officia nostrum maiores quia mollitia? 
+                    Delectus facilis saepe dicta molestias.
+                    Aut, sunt laboriosam tenetur voluptatum
+                    cumque voluptate maximeconsequatur nihil 
+                    delectus accusantiumofficia nostrum maiores
+                    quia mollitia? Delectus facilis saepe dicta 
+                    molestias. Aut, sunt laboriosam 
+                    tenetur voluptatum cumque voluptate maxime
+                    consequatur nihil delectus accusantium
+                    officia nostrum maiores quia mollitia? 
+                    Delectus facilis saepe dicta molestias.
+                    Aut, sunt laboriosam tenetur voluptatum
+                    cumque voluptate maximeconsequatur nihil 
+                    delectus accusantiumofficia nostrum maiores
+                    quia mollitia? Delectus facilis saepe dicta 
+                    molestias.
+                </p>
+        </article>
+    );
+}
