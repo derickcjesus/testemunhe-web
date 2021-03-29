1af9f28b0fae609d7ea2187d7edd2ab0fa672b36
Derick de Jesus <derickcjesus@gmail.com>
2021-03-29 11:48:48 -0300

    refactor: renames Testify component to TestifyList

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import TestifyForm from "./pages/TestifyForm";
-import Testify from "./pages/Testify";
+import TestifyList from "./pages/TestifyList";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={LandingPage} />
       <Route path="/testify-form" component={TestifyForm} />
-      <Route path="/testify" component={Testify} />
+      <Route path="/testify" component={TestifyList} />
     </BrowserRouter>
   );
 }
