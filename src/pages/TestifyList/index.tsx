adeba2d26325bdec063a7f6c5fb9a9628df1ee3d
Derick de Jesus <derickcjesus@gmail.com>
2021-04-24 10:55:25 -0300

    feat: shows results

--- src/pages/TestifyList/index.tsx
+++ src/pages/TestifyList/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../../components/PageHeader';
-import TestifyItem from '../../components/TestifyItem';
+import TestifyItem, { Testimony } from '../../components/TestifyItem';
+import api from '../../services/api';
 
 import './styles.css'
 
 export default function TestifyList() {
+    const [name] = useState(" ");
+    const [age] = useState(" ");
+    const [testimony] = useState(" ");
+
+    const [testimonials, setTestimonials] = useState([]);
+
+    async function searchTestimony() {
+        const response = await api.get('testify-list', {
+            params: {
+                name,
+                age,
+                testimony,
+            },
+        });
+    
+        setTestimonials(response.data);
+    }
+
+    searchTestimony();
+    
+
     return (
         <>
         <div id="page-testify-list">
         <PageHeader title="Veja aqui alguns testemunhos" />
 
             <main>
-                <TestifyItem />
-                <TestifyItem />
-
+                {testimonials.map((testimony: Testimony) => {
+                    return <TestifyItem key={testimony.id} testimonys={testimony}/>;
+                })}
             </main>
         </div>
         </>
