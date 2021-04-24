61765e8f9569805b3ac8cc6b7fc636a6ce74a039
Derick de Jesus <derickcjesus@gmail.com>
2021-04-24 10:54:45 -0300

    feat: inserts form data into the bank

--- src/pages/TestifyForm/index.tsx
+++ src/pages/TestifyForm/index.tsx
@@ -1,42 +1,91 @@
-import React from 'react'
+import React, { FormEvent, useState } from "react";
+import { useHistory } from "react-router-dom";
+import PageHeader from "../../components/PageHeader";
+import api from "../../services/api";
 
-import './styles.css'
-
-import PageHeader from '../../components/PageHeader';
+import "./styles.css";
 
 export default function TestifyForm() {
-    return (
-        <div className="testify-form-page">
-            <PageHeader 
-                title="Conte ao mundo o seu testemunho"
-                description="Para isto basta preecher este formulário"
-            />
+  const history = useHistory();  
 
-            <main>
-                <fieldset>
-                    <legend>Seus dados</legend>
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="age">Idade</label>
-                        <input type="text" id="age"/>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Seu Testemunho</legend>
-                    <div className="input-block">
-                        <label htmlFor="testimony">Escreva aqui seu testemunho</label>
-                        <textarea name="testimony" id="testimony"></textarea>
-                    </div>
-                </fieldset>
-                <footer>                      
-                    <p>Preencha todos os dados antes de salvar</p>
+  const [name, setName] = useState(" ");
+  const [age, setAge] = useState(" ");
+  const [testimony, setTestimony] = useState(" ");
 
-                    <button type="button">Salvar testemunho</button>
-                </footer>
-            </main>
-        </div>
-    );
+  function handleCreateTestimony(e: FormEvent) {
+    e.preventDefault();
+
+    api
+      .post('create-testimony', {
+        name,
+        age,
+        testimony,
+      })
+      .then(() => {
+        alert("Testemunho cadastrado com sucesso!");
+
+        history.push('/')
+      })
+      .catch(() => {
+        alert("Ouve algum ERRO no cadastro do seu testemunho");
+      });
+  }
+
+  return (
+    <div className="testify-form-page">
+      <PageHeader
+        title="Conte ao mundo o seu testemunho"
+        description="Para isto basta preecher este formulário"
+      />
+
+      <main>
+        <form onSubmit={handleCreateTestimony}>
+          <fieldset>
+            <legend>Seus dados</legend>
+            <div className="input-block">
+              <label htmlFor="name">Nome completo</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="age">Idade</label>
+              <input
+                type="text"
+                id="age"
+                value={age}
+                onChange={(e) => {
+                  setAge(e.target.value);
+                }}
+              />
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Seu Testemunho</legend>
+            <div className="input-block">
+              <label htmlFor="testimony">Escreva aqui seu testemunho</label>
+              <textarea
+                name="testimony"
+                id="testimony"
+                value={testimony}
+                onChange={(e) => {
+                  setTestimony(e.target.value);
+                }}
+              ></textarea>
+            </div>
+          </fieldset>
+          <footer>
+            <p>Preencha todos os dados antes de salvar</p>
+
+            <button type="submit">Salvar testemunho</button>
+          </footer>
+        </form>
+      </main>
+    </div>
+  );
 }
